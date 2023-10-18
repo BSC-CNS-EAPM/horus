84c44f5f70c359265218c23c2d89f25e4f8dd96d
Christian Dominguez <christian.99dominguez@gmail.com>
2023-10-18 16:33:06 +0200

    Added file and folder check for input blocks Folder and File

--- AppSupport/DefaultPlugins/Horus/Include/Blocks/File.py
+++ AppSupport/DefaultPlugins/Horus/Include/Blocks/File.py
@@ -1,3 +1,4 @@
+import os
 from HorusAPI import PluginVariable, InputBlock, VariableTypes
 
 fileVariable = PluginVariable(
@@ -8,7 +9,23 @@ fileVariable = PluginVariable(
     allowedValues=["*"],
 )
 
+
+def checkPathIsFile(block: InputBlock):
+    fileVariableValue = block.inputs.get("file", None)
+
+    if fileVariableValue is None:
+        raise Exception("No file provided.")
+
+    if not os.path.exists(fileVariableValue):
+        raise Exception("Provided path does not exist.")
+
+    if not os.path.isfile(fileVariableValue):
+        raise Exception("Provided path is not a file.")
+
+    block.setOutput("file", fileVariableValue)
+
+
 # Create the block "File"
 fileBlock = InputBlock(
-    "File", description="Select a single file", action=None, variable=fileVariable
+    "File", description="Select a single file", action=checkPathIsFile, variable=fileVariable
 )
--- AppSupport/DefaultPlugins/Horus/Include/Blocks/Folder.py
+++ AppSupport/DefaultPlugins/Horus/Include/Blocks/Folder.py
@@ -1,5 +1,5 @@
+import os
 from HorusAPI import PluginVariable, InputBlock, VariableTypes
-
 
 folderVariable = PluginVariable(
     name="Folder",
@@ -8,7 +8,23 @@ folderVariable = PluginVariable(
     type=VariableTypes.FOLDER,
 )
 
+
+def checkPathIsFolder(block: InputBlock):
+    folderVariableValue = block.inputs.get("folder", None)
+
+    if folderVariableValue is None:
+        raise Exception("No folder provided.")
+
+    if not os.path.exists(folderVariableValue):
+        raise Exception("Provided path does not exist.")
+
+    if not os.path.isdir(folderVariableValue):
+        raise Exception("Provided path is not a folder.")
+
+    block.setOutput("folder", folderVariableValue)
+
+
 # Create the block "Folder"
 folderBlock = InputBlock(
-    "Folder", description="Select a folder", action=None, variable=folderVariable
+    "Folder", description="Select a folder", action=checkPathIsFolder, variable=folderVariable
 )
