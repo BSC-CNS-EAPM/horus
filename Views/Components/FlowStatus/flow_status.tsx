992ce59169f786a91fd9cc1ece6424228740e96e
Christian Domínguez <34599976+chdominguez@users.noreply.github.com>
2023-11-06 15:10:02 +0100

    Added failed flow status view (#103)

--- Views/Components/FlowStatus/flow_status.tsx
+++ Views/Components/FlowStatus/flow_status.tsx
@@ -17,6 +17,8 @@ function FlowStatusView(props: FlowStatusViewProps) {
       return <StoppedFlowStatus />;
     case FlowStatus.FINISHED:
       return <FinishedFlowStatus />;
+    case FlowStatus.ERROR:
+      return <ErrorFlowStatus />;
     default:
       return null;
   }
@@ -123,6 +125,28 @@ function FinishedFlowStatus() {
         />
       </svg>
       Finished
+    </FlowStatusBase>
+  );
+}
+
+function ErrorFlowStatus() {
+  return (
+    <FlowStatusBase color="red">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-5 h-5"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+        />
+      </svg>
+      Failed
     </FlowStatusBase>
   );
 }
