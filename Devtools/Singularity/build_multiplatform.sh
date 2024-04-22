e1bd69b54c5b4fb32a26ecac5db7980ff43fbab8
Christian Domínguez <christian.99dominguez@gmail.com>
2024-04-22 11:06:14 +0200

    Updated build script to support centos

--- Devtools/Singularity/build_multiplatform.sh
+++ Devtools/Singularity/build_multiplatform.sh
@@ -11,30 +11,35 @@
 
 # Clean the build folder using the singularity image
 echo "Running bun run clean-all using Node from Rocky image..."
-singularity exec --bind .:/ docker://chdominguez/horus_rocky bun run clean-all
+singularity exec --bind .:/app docker://chdominguez/horus_rocky bun run clean-all
 
 # Build Horus for Rocky Linux 8
 echo "Building Horus for Rocky Linux 8..."
-singularity run --bind .:/ docker://chdominguez/horus_rocky
+singularity run --bind .:/app docker://chdominguez/horus_rocky
 echo "Finished building Horus for Rocky Linux 8"
 
 # Clean the compiled files
 echo "Running bun run clean-build using Node from Rocky image..."
-singularity exec --bind .:/ docker://chdominguez/horus_rocky bun run clean-build
+singularity exec --bind .:/app docker://chdominguez/horus_rocky bun run clean-build
 
 # Build for Ubuntu 22.04
 echo "Building Horus for Ubuntu 22.04..."
-singularity run --bind .:/ docker://chdominguez/horus_ubuntu
+singularity run --bind .:/app docker://chdominguez/horus_ubuntu
 echo "Finished building Horus for Ubuntu 22.04"
 
 # Clean the compiled files
 echo "Running bun run clean-build using Node from Rocky image..."
-singularity exec --bind .:/ docker://chdominguez/horus_ubuntu bun run clean-build
+singularity exec --bind .:/app docker://chdominguez/horus_ubuntu bun run clean-build
 
 # Build for Ubuntu 14.04
-echo "Building Horus on Ubuntu 14.04 (Universal linux, no-gui)..."
-singularity run --bind .:/ docker://chdominguez/horus_universal
+echo "Building Horus on Ubuntu 14.04 (Universal linux, no-pywebview)..."
+singularity run --bind .:/app docker://chdominguez/horus_universal
 echo "Finished building Horus for Ubuntu 14.04"
+
+# Build for CentOS 7
+echo "Building Horus on CentOS 7 (no-pywebview)..."
+singularity run --bind .:/app docker://chdominguez/horus_centos
+echo "Finished building Horus for CentOS 7"
 
 echo "\n"
 echo "==============================================="
